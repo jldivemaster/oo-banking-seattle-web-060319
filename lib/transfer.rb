6b4d28401f566d4e1b7bda1f089b05632d961949
Jason Leach <jlspursfan@gmail.com>
2019-06-08 18:38:45 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -30,8 +30,8 @@ class Transfer
 
   def reverse_transfer
     if @status == 'complete'
-      sender.balance += @amount
-      receiver.balance -= @amount
+      sender.balance -= @amount
+      receiver.balance += @amount
       @status = 'reversed'
     end
   end
